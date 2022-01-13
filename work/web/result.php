35aa65202316caba2fa87a3114fdf57bfcc9eccb
ryo0705s <inspire.sasaki15@gmail.com>
2022-01-13 20:47:57 +0900

    postの命令を受けて投稿されたか＝アドレス直打アクセスなどを弾く　ための記述を追加

--- work/web/result.php
+++ work/web/result.php
@@ -3,12 +3,17 @@
 require('../app/functions.php');
 
 include('../app/_parts/_header.php');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $message = filter_input(INPUT_POST, 'message');
+$message = $message != "" ? $message : "...";
 $filename = '../app/messages.txt';
 // $messages = file($filename, FILE_IGNORE_NEW_LINES);
 $fp = fopen($filename, 'a');
 fwrite($fp, $message . "\n");
 fclose($fp);
+} else {
+exit("invalid request");
+}
 ?>
 
   <p>got it !</p>
